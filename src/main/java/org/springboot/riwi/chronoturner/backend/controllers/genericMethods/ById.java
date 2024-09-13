6bb043d3f337c07e54933629831d062b5bc16d84
Santiago <santimh04@gmail.com>
2024-09-13 17:32:52 -0500

    feature: controllers structure determined

--- src/main/java/org/springboot/riwi/chronoturner/backend/controllers/genericMethods/ById.java
+++ src/main/java/org/springboot/riwi/chronoturner/backend/controllers/genericMethods/ById.java
@@ -3,6 +3,5 @@ package org.springboot.riwi.chronoturner.backend.controllers.genericMethods;
 import org.springframework.http.ResponseEntity;
 
 public interface ById<ID, Entity> {
-
     public ResponseEntity<Entity> ById(ID id);
 }
