200c76cfdcffe9233f0a6b6b5f351a0fa92bf91e
Santiago <santimh04@gmail.com>
2024-09-13 17:11:56 -0500

    feature: TaskController file creation. Please implement on a new branch

--- src/main/java/org/springboot/riwi/chronoturner/backend/controllers/controllerImplementations/TaskController.java
+++ src/main/java/org/springboot/riwi/chronoturner/backend/controllers/controllerImplementations/TaskController.java
@@ -1,5 +1,6 @@
 package org.springboot.riwi.chronoturner.backend.controllers.controllerImplementations;
 
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springboot.riwi.chronoturner.backend.services.entityServices.TaskService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -7,7 +8,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/task")
-public class TaskController {
+@Tag(name = "Goals")
+public class TaskController {   //Aquí falta también implementar el controlador deseado, cuidadongos
         //Atributos de TaskController
     @Autowired
     private TaskService service;
